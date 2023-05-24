a193113a234e7ddb7408eb26c78dd29b846b04e4
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-05-24 22:02:18 +0700

    Menambah keterangan siswa perkenalan

--- ParadigmaOOP2/ParadigmaOOP2.cpp
+++ ParadigmaOOP2/ParadigmaOOP2.cpp
@@ -34,3 +34,14 @@ public:
 		sekolah(pSekolah) {
 		cout << "pelajar dibuat\n" << endl;
 	}
+};
+
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
+
+
