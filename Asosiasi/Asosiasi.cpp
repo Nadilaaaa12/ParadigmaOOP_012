4cbaa619524ffdd844fe6bbaab57751d23296a5c
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-05-24 22:29:36 +0700

    Menambahkan keterangan jantung

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -8,7 +8,7 @@ public:
 	string nama;
 	vector<dokter*> daftar_dokter;
 	pasien(string pNama) :nama(pNama) {
-		cout << "Pasien" \"" << nama << "\" ada\n";
+		cout << "Pasien" "" << nama << "\" ada\n";
 	}
 	~pasien() {
 		cout << "Pasien \"" << nama << "\" tidak ada\n";
@@ -41,3 +41,35 @@ void pasien::cetakDokter() {
 		for (auto& a : daftar_dokter) {
 			cout << a->nama << "\n";
 		}
+	    cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDokter(this);
+}
+void dokter::tambahPasien(pasien* pPasien) {
+	cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n"
+		for (auto& a : daftar_pasien) {
+			cout << a->nama << "\n";
+		}
+	    cout << endl;
+}
+
+int main() {
+	dokter* varDokter1 = new dokter("dr.Budi");
+	dokter* varDokter2 = new dokter("dr.Tono");
+	pasien* varPasien1 = new pasien("Andi");
+	pasien* varPasien2 = new pasien("Lia");
+
+	varDokter1->tambahPasien(varPasien1);
+	varDokter1->tambahPasien(varPasien2);
+	varDokter2->tambahPasien(varPasien1);
+
+	varDokter1->cetakPasien();
+	varDokter2->cetakPasien();
+	varPasien1->cetakDokter();
+	varPasien2->cetakDokter();
+
+
+}
+
