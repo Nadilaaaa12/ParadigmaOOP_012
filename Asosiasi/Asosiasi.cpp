313589f7fa1a0fcdd733475a041dd258c1abc7ce
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-05-24 22:19:31 +0700

    Menampilkan Keterangan Dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -21,3 +21,18 @@ class dokter {
 public:
 	string nama;
 	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+
+	void tambahPasien(pasien*);
+	void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
